9de74464295ddc756a16f27de2cfcfdd5302a31b
Alex-Degtyarenko <aleksdegtyarenko20@gmail.com>
2024-08-24 12:51:51 +0400

    Initial commit

---
+++ src/main/java/net/lancet_/realhungry/RealHungryClient.java
@@ -0,0 +1,11 @@
+package net.lancet_.realhungry;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RealHungryClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
